14f54fb6ecdff7bc75ff2c5a0e63ee06d3b14805
Mohammad Dahab <mdit1995@gmail.com>
2019-01-31 13:53:47 +0300

    Add todo logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    todos: [],
+    newTodo: '',
+  }
+
+  onTodoChange = evt => this.setState({ newTodo: evt.target.value })
+
+  addTodo = () => this.setState(currentState => ({ newTodo: '', todos: [...currentState.todos, currentState.newTodo] }))
+
+  deleteTodo = item => this.setState(currentState => ({ todos: currentState.todos.filter(todo => todo !== item ) }))
+
   render() {
     return (
-      <div className="font-sans">
-        <div className="flex justify-center">
-          <h1 className="text-indigo font-light mt-4">All set! <span role="img" aria-label="thumbs up">👍</span></h1>
+      <div class="container mx-auto">
+        <h1>TODO App!</h1>
+        <div>
+          <input class="border" value={this.state.newTodo} onChange={this.onTodoChange} />
+          <button onClick={this.addTodo}>Add</button>
+          <div>
+            <ul>
+              {
+                this.state.todos.map(todo => <li onClick={() => this.deleteTodo(todo)}>{todo}</li>)
+              }
+            </ul>
+          </div>
         </div>
       </div>
     );
